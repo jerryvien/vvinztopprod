3f1f2b9ecab9f5baf7ad0a92f6b350eaf5ec3fd6
jerryvien <jerryyee0902@gmail.com>
2025-06-16 18:01:11 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,81 +1,64 @@
-  <footer id="footer" class="footer light-background">
+<footer id="footer" class="footer light-background">
 
-    <div class="container footer-top">
-      <div class="row gy-4">
-        <div class="col-lg-4 col-md-6 footer-about">
-          <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">MyWebsite</span>
-          </a>
-          <div class="footer-contact pt-3">
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
-          </div>
-          <div class="social-links d-flex mt-4">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
-          </div>
+  <div class="container footer-top">
+    <div class="row gy-4">
+      
+      <!-- Company Info -->
+      <div class="col-lg-4 col-md-6 footer-about">
+        <a href="index.php" class="logo d-flex align-items-center">
+          <span class="sitename">VVINZ TOP CELLSPARE</span>
+        </a>
+        <div class="footer-contact pt-3">
+          <p>Level 5, Selayang Mall</p>
+          <p>Jalan SU 9, Taman Selayang Utama,<br>68100 Batu Caves, Selangor</p>
+          <p class="mt-3"><strong>Phone:</strong> <a href="https://wa.me/60102899611" target="_blank">+60 10-289 9611</a></p>
+          <p><strong>Email:</strong> <a href="mailto:support@vvinztop.com">support@vvinztop.com</a></p>
         </div>
-
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Terms of service</a></li>
-            <li><a href="#">Privacy policy</a></li>
-          </ul>
+        <div class="social-links d-flex mt-4">
+          <a href="#"><i class="bi bi-facebook"></i></a>
+          <a href="#"><i class="bi bi-instagram"></i></a>
+          <a href="#"><i class="bi bi-tiktok"></i></a>
         </div>
-
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Graphic Design</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Hic solutasetp</h4>
-          <ul>
-            <li><a href="#">Molestiae accusamus iure</a></li>
-            <li><a href="#">Excepturi dignissimos</a></li>
-            <li><a href="#">Suscipit distinctio</a></li>
-            <li><a href="#">Dilecta</a></li>
-            <li><a href="#">Sit quas consectetur</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Nobis illum</h4>
-          <ul>
-            <li><a href="#">Ipsam</a></li>
-            <li><a href="#">Laudantium dolorum</a></li>
-            <li><a href="#">Dinera</a></li>
-            <li><a href="#">Trodelas</a></li>
-            <li><a href="#">Flexo</a></li>
-          </ul>
-        </div>
-
       </div>
-    </div>
 
-    <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">MyWebsite</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      <!-- Useful Links -->
+      <div class="col-lg-2 col-md-3 footer-links">
+        <h4>快速导航</h4>
+        <ul>
+          <li><a href="index.php">首页</a></li>
+          <li><a href="#about">关于我们</a></li>
+          <li><a href="#hero">我们的优势</a></li>
+          <li><a href="#branches">门市地址</a></li>
+          <li><a href="#contact">联络我们</a></li>
+        </ul>
       </div>
-    </div>
 
-  </footer>
+      <!-- Services -->
+      <div class="col-lg-2 col-md-3 footer-links">
+        <h4>配件类别</h4>
+        <ul>
+          <li><a href="#">LCD 屏幕</a></li>
+          <li><a href="#">电池 Battery</a></li>
+          <li><a href="#">排线 & 主板</a></li>
+          <li><a href="#">后盖 Housing</a></li>
+          <li><a href="#">充电尾插 / 镜头等</a></li>
+        </ul>
+      </div>
+
+      <!-- Empty for future use or promo -->
+      <div class="col-lg-4 col-md-6 footer-newsletter text-md-end">
+        <h4>快速订货？</h4>
+        <p>点击 <a href="https://wa.me/60102899611" target="_blank"><strong>WhatsApp</strong></a> 即刻联系我们，我们团队将快速处理您的配件订单。</p>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="container copyright text-center mt-4">
+    <p>© <strong class="sitename">VVINZ TOP CELLSPARE</strong> 版权所有 · All Rights Reserved</p>
+    <div class="credits">
+      Designed & Powered by <a href="https://navbright.com" target="_blank">Navbright Technology</a>
+    </div>
+  </div>
+
+</footer>
