0246736f3a5e0c7e10f18729df2455bb2730caa2
jerryvien <jerryyee0902@gmail.com>
2025-06-16 17:14:48 +0800

    commit all changes

--- index.php
+++ index.php
@@ -118,52 +118,6 @@
       </section>
     <!-- /Hero Section -->
 
-    <!-- About Section -->
-      <section id="about" class="about section">
-
-        <div class="container">
-
-          <div class="row gy-4">
-
-            <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
-              <p class="who-we-are">关于我们</p>
-              <h3>从零件批发走向全国分店的发展之路</h3>
-              <p class="fst-italic">
-                VVINZ TOP CELLSPARE S/B 于2014年成立，专注于手机内配件批发，凭借稳定品质与快速服务，在行业中建立了广泛信任与合作基础。
-              </p>
-              <ul>
-                <li><i class="bi bi-check-circle"></i> <span>四家分店（Sungai Buloh、Rawang、Subang、Kepong）提供就近配送。</span></li>
-                <li><i class="bi bi-check-circle"></i> <span>支持 iPhone、Samsung、Huawei、Xiaomi 等主流品牌全系列机型。</span></li>
-                <li><i class="bi bi-check-circle"></i> <span>30分钟极速送达，保障维修门市效率，客户满意度显著提升。</span></li>
-              </ul>
-              <a href="#" class="read-more"><span>了解更多</span><i class="bi bi-arrow-right"></i></a>
-            </div>
-
-            <div class="col-lg-6 about-images" data-aos="fade-up" data-aos-delay="200">
-              <div class="row gy-4">
-                <div class="col-lg-6">
-                  <img src="assets/img/about/about-portrait-1.webp" class="img-fluid" alt="">
-                </div>
-                <div class="col-lg-6">
-                  <div class="row gy-4">
-                    <div class="col-lg-12">
-                      <img src="assets/img/about/about-8.webp" class="img-fluid" alt="">
-                    </div>
-                    <div class="col-lg-12">
-                      <img src="assets/img/about/about-12.webp" class="img-fluid" alt="">
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-      </section>
-    <!-- /About Section -->
-
-
     <!-- How We Work Section -->
       <section id="how-we-work" class="how-we-work section">
 
@@ -411,10 +365,6 @@
         </div>
       </section>
     <!-- /Stats Section -->
-
-
-
-
 
 
     <!-- Team Section -->
