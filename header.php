a8e21f6f7a398c58d14563106fb64a78fdf94b12
jerryvien <jerryyee0902@gmail.com>
2025-06-25 01:04:59 +0800

    modify the header access

--- header.php
+++ header.php
@@ -9,8 +9,8 @@
 
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="index.php" class="active">Home</a></li>
-          <li><a href="about.php">About</a></li>
+          <li><a href="index" class="active">Home</a></li>
+          <li><a href="about">About</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#team">Team</a></li>
